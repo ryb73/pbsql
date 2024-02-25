7495dce0221001c9aba8ce57ad1939ba066c9ff1
Ryan <rpbiwer@gmail.com>
2024-02-24 19:57:05 -0500

    Cabinet -> pbsql

--- build-wasm-nodejs.sh
+++ build-wasm-nodejs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm -rf pkg-nodejs pkg-bundler
 wasm-pack build --no-pack --release --target nodejs -d pkg-nodejs
-mv pkg-nodejs/cabinet_sql.js pkg-nodejs/cabinet_sql.cjs
+mv pkg-nodejs/pbsql.js pkg-nodejs/pbsql.cjs
 wasm-pack build --no-pack --release --target bundler -d pkg-bundler
