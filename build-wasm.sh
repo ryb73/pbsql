abd2df9c591266c9f4ed0db28fd5cc6b55bf54ab
Ryan <rpbiwer@gmail.com>
2024-04-03 14:23:32 -0400

    Fix bundler build

--- build-wasm.sh
+++ build-wasm.sh
@@ -2,8 +2,8 @@
 rm -rf pkg-nodejs pkg-bundler
 
 wasm-pack build --no-pack --release --target nodejs -d pkg-nodejs
-rm pkg-bundler/.gitignore
+rm pkg-nodejs/.gitignore
 mv pkg-nodejs/pbsql.js pkg-nodejs/pbsql.cjs
 
 wasm-pack build --no-pack --release --target bundler -d pkg-bundler
-rm pkg-nodejs/.gitignore
+rm pkg-bundler/.gitignore
